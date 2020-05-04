e0fc9da68bbfb3e1cd95a12d3881c354f79a5c87
Byeonggon Lee <gonny952@gmail.com>
2020-05-04 13:45:50 +0900

    Add whitespace lexer code

---
+++ lexer.py
@@ -0,0 +1,33 @@
+import sys
+from lexer.table import table
+from lexer.tokens import tokens
+
+def lex(infile):
+    state = 0
+    for line in infile:
+        for char in line:
+            while True:
+                print('char', char)
+                print('state', state)
+                # if able to proceed
+                if char in table[state]: 
+                    state = table[state][char]
+                # if state is final state and unable to proceed
+                elif state in tokens:
+                    print('token', tokens[state])
+                    continue
+                # if state is not final state and unable to proceed
+                else: 
+                    print('failed to lex')
+                    return False
+                break
+
+    if state in tokens:
+        print('token', tokens[state])
+        return True
+    else:
+        print('failed to lex')
+        return False
+
+with open(sys.argv[1], 'r') as infile:
+    lex(infile)
---
+++ lexer/tokens.py
@@ -0,0 +1,1 @@
+tokens = {19: 'WSPACE'}

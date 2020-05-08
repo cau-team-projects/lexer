1ad02a39860c346106b009bea80f83b1499b1a92
Byeonggon Lee <gonny952@gmail.com>
2020-05-09 07:45:16 +0900

    Add test inputs

--- lexer.py
+++ lexer.py
@@ -1,35 +1,45 @@
 import sys
-from lexer.table import table
-from lexer.tokens import tokens
+import lexer.table
+import lexer.tokens
 
-def lex(infile):
-    state = 0
-    for line in infile:
-        for char in line:
-            while True:
-                print('char', char)
-                print('cur state', state)
-                # if able to proceed
-                if char in table[state]: 
-                    state = table[state][char]
-                    print('next state', state)
-                # if state is final state and unable to proceed
-                elif state in tokens:
-                    print('token', tokens[state])
-                    state = 0
-                    continue
-                # if state is not final state and unable to proceed
-                else: 
-                    print('failed to lex')
-                    return False
-                break
-
-    if state in tokens:
-        print('token', tokens[state])
-        return True
-    else:
-        print('failed to lex')
-        return False
+class Lexer:
+    def __init__(self):
+        self.state = 0
+        self.charbuf = []
+        self.table = lexer.table.table
+        self.tokens = lexer.tokens.tokens
+    def prtoken(self):
+        print('<%s, %s>' % (self.tokens[self.state], ''.join(self.charbuf[:-1])))
+    def lex(self, infile):
+        for line in infile:
+            self.charbuf = []
+            for char in line:
+                self.charbuf.append(char)
+                while True:
+                    print('char', char)
+                    print('cur state', self.state)
+                    # if able to proceed
+                    if char in self.table[self.state]: 
+                        self.state = self.table[self.state][char]
+                        print('next state', self.state)
+                    # if state is final state and unable to proceed
+                    elif self.state in self.tokens:
+                        self.prtoken()
+                        self.state = 0
+                        self.charbuf = self.charbuf[-1:]
+                        continue
+                    # if state is not final state and unable to proceed
+                    else: 
+                        print('failed to lex')
+                        return False
+                    break
+        if self.state in self.tokens:
+            self.prtoken()
+            return True
+        else:
+            print('failed to lex')
+            return False
 
 with open(sys.argv[1], 'r') as infile:
-    lex(infile)
+    lexer = Lexer()
+    lexer.lex(infile)
